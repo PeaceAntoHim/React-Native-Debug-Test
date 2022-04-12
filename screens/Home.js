6811d3bb4465e3f3eb57b1de2fc69b88e365f499
PeaceAntoHim <stefanusfranssebastian@gmail.com>
2022-04-12 17:31:13 +0700

    tambahin fiture refresh

--- screens/Home.js
+++ screens/Home.js
@@ -12,10 +12,6 @@ import {
   Button,
 } from 'react-native';
 
-//import all the components we are going to use.
-import Geolocation from '@react-native-community/geolocation';
-
-
 const Home = ({ navigation }) => {
 
   // Data Phone system
@@ -45,98 +41,7 @@ const Home = ({ navigation }) => {
     requestPhonePermission();
   }, []);
 
-    let deviceJSON = {}
-    //Data geolocation service
-    const [currentLongitude, setCurrentLongitude] = useState('');
-    const [currentLatitude, setCurrentLatitude] = useState('');
-    deviceJSON.latitude = parseFloat(currentLatitude);
-    deviceJSON.longitude = parseFloat(currentLongitude);
-    // console.log(deviceJSON.latitude);
-    // console.log(deviceJSON.longitude);
- 
-    useEffect(() => {
-       const requestLocationPermission = async () => {
-          if (Platform.OS === 'ios') {
-             getOneTimeLocation();
-             subscribeLocationLocation();
-          } else {
-             try {
-                const granted = await PermissionsAndroid.request(
-                   PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
-                   {
-                      title: 'Location Access Required',
-                      message: 'This App needs to Access your location',
-                      buttonNeutral: "Ask Me Later",
-                      buttonNegative: "Cancel",
-                      buttonPositive: "OK"
-                   },
-                );
-                if (granted === PermissionsAndroid.RESULTS.GRANTED) {
-                   //To Check, If Permission is granted
-                   getOneTimeLocation();
-                   subscribeLocationLocation();
-                } else {
-                   setLocationStatus('Permission Denied');
-                } 
-             } catch (err) {
-                console.warn(err);      
-             }
-          }
-       };
-       requestLocationPermission();
-       return () => {
-          Geolocation.clearWatch(watchID);
-       };
-    }, []);
-
-    const getOneTimeLocation = () => {
-      Geolocation.getCurrentPosition(
-      //Will give you the current location
-      (position) => {
-         //getting the Longitude from the location json
-         const currentLongitude = position.coords.longitude;
-         //getting the Latitude from the location json
-         const currentLatitude = position.coords.latitude;
-         //Setting Longitude state
-         setCurrentLongitude(currentLongitude);
-         //Setting Latitude state
-         setCurrentLatitude(currentLatitude);
-      },
-      (err) => {
-         // setLocationStatus(error.message);
-         console.warn(err);
-      },
-      {
-         enableHighAccuracy: false,
-         timeout: 30000,
-         maximumAge: 1000
-      },
-      );
-   };
-
-   const subscribeLocationLocation = () => {
-      watchID = Geolocation.watchPosition(
-      (position) => {
-         //Will give you the location on location change
-         //getting the Longitude from the location json        
-         const currentLongitude = position.coords.longitude;
-         //getting the Latitude from the location json
-         const currentLatitude = position.coords.latitude;
-         //Setting Longitude state
-         setCurrentLongitude(currentLongitude);
-         //Setting Latitude state
-         setCurrentLatitude(currentLatitude);
-      },
-      (err) => {
-         console.warn(err);
-      },
-      {
-         enableHighAccuracy: false,
-         maximumAge: 1000
-      },
-      );
-   };
-   console.log(deviceJSON.latitude);
+    
 
   return (
    <SafeAreaView style={{flex: 1}}>
@@ -164,20 +69,22 @@ const Home = ({ navigation }) => {
               justifyContent: 'center',
               alignItems: 'center',
               marginTop: 16,
-            }}>
+            }}
+          >
+            Scan Now
           </Text>
           <View style={{marginTop: 20}}>
             <Button
               title="Button"
-              onPress={() =>{
-                if(currentLatitude != '' && currentLongitude != '') {
-                  navigation.navigate('Check', {
-                      paramKey: deviceJSON
-                  });
-                } else {
-                    alert("Please wait for a moment")
-                  }}
-                }
+              onPress={() => navigation.navigate('Scan')}
+                // if(currentLatitude != '' && currentLongitude != '') {
+                //   navigation.navigate('Scan', {
+                //       paramKey: deviceJSON
+                //   });
+                // } else {
+                //     alert("Please wait for a moment")
+                //   }}
+                // }
               />
           </View>
         </View>
--- screens/Scan.js
+++ screens/Scan.js
@@ -1,11 +1,158 @@
-import React from 'react';
-import {View, StyleSheet, Text} from 'react-native';
+//import all the components we are going to use.
+import React, { useState, useEffect } from 'react';
+import {
+   View,
+   StyleSheet,
+   SafeAreaView,
+   PermissionsAndroid,
+   RefreshControl,
+   ScrollView,
+   ActivityIndicator
+} from 'react-native';
+import QRCodeScanner from 'react-native-qrcode-scanner';
+import Geolocation from '@react-native-community/geolocation';
 
-const Scan = () => {
+const wait = (timeout) => {
+   return new Promise(resolve => {
+      setTimeout(resolve, timeout);
+   });
+}
+
+const Scan = ({ navigation }) => {
+   // const [scan, setScanState] = useState(false);
+   // const [result, setResult] = useState();
+
+   let deviceJSON = {}
+    //Data geolocation service
+    const [currentLongitude, setCurrentLongitude] = useState('');
+    const [currentLatitude, setCurrentLatitude] = useState('');
+    deviceJSON.latitude = parseFloat(currentLatitude);
+    deviceJSON.longitude = parseFloat(currentLongitude);
+    // console.log(deviceJSON.latitude);
+    // console.log(deviceJSON.longitude);
+ 
+    useEffect(() => {
+       const requestLocationPermission = async () => {
+          if (Platform.OS === 'ios') {
+             getOneTimeLocation();
+             subscribeLocationLocation();
+          } else {
+             try {
+                const granted = await PermissionsAndroid.request(
+                   PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+                   {
+                      title: 'Location Access Required',
+                      message: 'This App needs to Access your location',
+                      buttonNeutral: "Ask Me Later",
+                      buttonNegative: "Cancel",
+                      buttonPositive: "OK"
+                   },
+                );
+                if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+                   //To Check, If Permission is granted
+                   getOneTimeLocation();
+                   subscribeLocationLocation();
+                } else {
+                   setLocationStatus('Permission Denied');
+                } 
+             } catch (err) {
+                console.warn(err);      
+             }
+          }
+       };
+       requestLocationPermission();
+       return () => {
+          Geolocation.clearWatch(watchID);
+       };
+    }, []);
+
+    const getOneTimeLocation = () => {
+      Geolocation.getCurrentPosition(
+      //Will give you the current location
+      (position) => {
+         //getting the Longitude from the location json
+         const currentLongitude = position.coords.longitude;
+         //getting the Latitude from the location json
+         const currentLatitude = position.coords.latitude;
+         //Setting Longitude state
+         setCurrentLongitude(currentLongitude);
+         //Setting Latitude state
+         setCurrentLatitude(currentLatitude);
+      },
+      (err) => {
+         // setLocationStatus(error.message);
+         console.warn(err);
+      },
+      {
+         enableHighAccuracy: false,
+         // timeout: 40000,
+         maximumAge: 1000
+      },
+      );
+   };
+
+   const subscribeLocationLocation = () => {
+      watchID = Geolocation.watchPosition(
+      (position) => {
+         //Will give you the location on location change
+         //getting the Longitude from the location json        
+         const currentLongitude = position.coords.longitude;
+         //getting the Latitude from the location json
+         const currentLatitude = position.coords.latitude;
+         //Setting Longitude state
+         setCurrentLongitude(currentLongitude);
+         //Setting Latitude state
+         setCurrentLatitude(currentLatitude);
+      },
+      (err) => {
+         console.warn(err);
+      },
+      {
+         enableHighAccuracy: false,
+         maximumAge: 1000
+      },
+      );
+   };
+   // console.log(deviceJSON.latitude);
+   const [refreshing, setRefreshing] = useState(false);
+
+   const onRefresh = React.useCallback(() => {
+      setRefreshing(true);
+      wait(2000).then(() => setRefreshing(false));
+   }, [])
+
+   const [loading, setLoading] = useState(false);   
+
    return (
-      <View>
-         <Text style={styles.container}>Scan</Text>
-      </View>
+      // {if()}
+      <SafeAreaView style={styles.container}>
+         <View style={styles.sectionContainer}>
+            <ScrollView
+               refreshControl={
+                  <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
+               }
+            >
+            <QRCodeScanner
+               style={{ height: '100%' }} 
+               reactivate={true}
+               showMarker={true}
+               ref={(node) => { this.scanner = node }}
+               onRead={() => {
+                  if (currentLatitude != '' && currentLongitude != '') {
+                     navigation.navigate('Check', {
+                        paramKey: deviceJSON
+                     });
+                     } else {
+                        <View style={[styles.container, styles.horizontal]}>
+                           <ActivityIndicator size="large" color="#0000ff" />
+                        </View>
+                        // alert('Please wait for the location to be fetched');
+                     }}
+               } 
+            />
+            </ScrollView>
+         </View>
+      </SafeAreaView>
    );
 }
 
@@ -16,6 +163,14 @@ const styles = StyleSheet.create({
       padding: 10,
       alignItems: 'center',
       justifyContent: 'center',
+    },
+    sectionContainer: {
+      marginTop: -30,
+    },
+    horizontal: {
+      flexDirection: "row",
+      justifyContent: "space-around",
+      padding: 10
     }
 
 })
