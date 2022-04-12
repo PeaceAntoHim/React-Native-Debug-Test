050ea4dc25fa6b8a5c9de5a3e52787445551eb4c
PeaceAntoHim <stefanusfranssebastian@gmail.com>
2022-04-12 11:48:14 +0700

    code refactor

--- screens/Check.js
+++ screens/Check.js
@@ -1,12 +1,32 @@
 import React from 'react';
-import {SafeAreaView, View, StyleSheet, Text} from 'react-native';
-import Data from '../data/Device.js';
+import { View, StyleSheet, ScrollView, Text, SafeAreaView} from 'react-native';
+import DeviceInfo, {getManufacturerSync} from 'react-native-device-info';
 
-const Check = () => {
+const Check = ({route}) => {
+   let deviceJSON = {}
+   deviceJSON.uniqueId = DeviceInfo.getUniqueId();
+   deviceJSON.systemName = DeviceInfo.getSystemName();
+   deviceJSON.model = DeviceInfo.getModel();
+   deviceJSON.brand = DeviceInfo.getBrand();
+   deviceJSON.manufacturer = DeviceInfo.getManufacturerSync();
+   deviceJSON.deviceName = DeviceInfo.getDeviceNameSync();
+   deviceJSON.isLocationEnabled = DeviceInfo.isLocationEnabledSync();
+   deviceJSON.serialNumber = DeviceInfo.getSerialNumberSync();
+   deviceJSON.ipAddreas = DeviceInfo.getIpAddressSync();
+   deviceJSON.macAddress = DeviceInfo.getMacAddressSync();
+   deviceJSON.phoneNumber = DeviceInfo.getPhoneNumberSync();
+   deviceJSON.location = route.params.paramKey;
+
+   console.log(route.params.paramKey);
    return (
       <SafeAreaView style={styles.container}>
          <View>
-            <Data />
+            <Text style={styles.titleStyle}>info</Text>
+            <ScrollView>
+               <Text style={styles.instruction}>
+                  {JSON.stringify(deviceJSON, null, '  ')}
+               </Text>
+            </ScrollView>
          </View>
       </SafeAreaView>
    );
--- screens/Home.js
+++ screens/Home.js
@@ -1,5 +1,5 @@
 // import React in our code
-import React, {useEffect} from 'react';
+import React, { useEffect, useState} from 'react';
 
 // import all the components we are going to use
 import {
@@ -13,6 +13,8 @@ import {
 } from 'react-native';
 
 //import all the components we are going to use.
+import Geolocation from '@react-native-community/geolocation';
+
 
 const Home = ({ navigation }) => {
 
@@ -21,7 +23,7 @@ const Home = ({ navigation }) => {
   const requestPhonePermission = async () => {
     try {
         const granted = await PermissionsAndroid.request(
-          PermissionsAndroid.PERMISSIONS.READ_PHONE_NUMBERS,
+          PermissionsAndroid.PERMISSIONS.READ_PHONE_STATE,
           {
             title: "Need your phone number",
             message: "This app need you phone number to get attendance",
@@ -42,6 +44,98 @@ const Home = ({ navigation }) => {
     }
     requestPhonePermission();
   }, []);
+
+    let deviceJSON = {}
+    //Data geolocation service
+    const [currentLongitude, setCurrentLongitude] = useState('');
+    const [currentLatitude, setCurrentLatitude] = useState('');
+    deviceJSON.latitude = parseFloat(currentLatitude);
+    deviceJSON.longitude = parseFloat(currentLongitude);
+    // console.log(deviceJSON.latitude);
+    // console.log(deviceJSON.longitude);
+ 
+    useEffect(() => {
+       const requestLocationPermission = async () => {
+          if (Platform.OS === 'ios') {
+             getOneTimeLocation();
+             subscribeLocationLocation();
+          } else {
+             try {
+                const granted = await PermissionsAndroid.request(
+                   PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+                   {
+                      title: 'Location Access Required',
+                      message: 'This App needs to Access your location',
+                      buttonNeutral: "Ask Me Later",
+                      buttonNegative: "Cancel",
+                      buttonPositive: "OK"
+                   },
+                );
+                if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+                   //To Check, If Permission is granted
+                   getOneTimeLocation();
+                   subscribeLocationLocation();
+                } else {
+                   setLocationStatus('Permission Denied');
+                } 
+             } catch (err) {
+                console.warn(err);      
+             }
+          }
+       };
+       requestLocationPermission();
+       return () => {
+          Geolocation.clearWatch(watchID);
+       };
+    }, []);
+
+    const getOneTimeLocation = () => {
+      Geolocation.getCurrentPosition(
+      //Will give you the current location
+      (position) => {
+         //getting the Longitude from the location json
+         const currentLongitude = position.coords.longitude;
+         //getting the Latitude from the location json
+         const currentLatitude = position.coords.latitude;
+         //Setting Longitude state
+         setCurrentLongitude(currentLongitude);
+         //Setting Latitude state
+         setCurrentLatitude(currentLatitude);
+      },
+      (err) => {
+         // setLocationStatus(error.message);
+         console.warn(err);
+      },
+      {
+         enableHighAccuracy: false,
+         timeout: 30000,
+         maximumAge: 1000
+      },
+      );
+   };
+
+   const subscribeLocationLocation = () => {
+      watchID = Geolocation.watchPosition(
+      (position) => {
+         //Will give you the location on location change
+         //getting the Longitude from the location json        
+         const currentLongitude = position.coords.longitude;
+         //getting the Latitude from the location json
+         const currentLatitude = position.coords.latitude;
+         //Setting Longitude state
+         setCurrentLongitude(currentLongitude);
+         //Setting Latitude state
+         setCurrentLatitude(currentLatitude);
+      },
+      (err) => {
+         console.warn(err);
+      },
+      {
+         enableHighAccuracy: false,
+         maximumAge: 1000
+      },
+      );
+   };
 
   return (
    <SafeAreaView style={{flex: 1}}>
@@ -74,7 +168,14 @@ const Home = ({ navigation }) => {
           <View style={{marginTop: 20}}>
             <Button
               title="Button"
-              onPress={() => navigation.navigate('Check')}
+              onPress={() =>{
+                if(deviceJSON.latitude != null && deviceJSON.longitude != null)
+                { navigation.navigate('Check', {
+                    paramKey: deviceJSON})
+                } else {
+                  alert("Please wait for a moment")
+                }}
+              }
             />
           </View>
         </View>
